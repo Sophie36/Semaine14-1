73ef2cca6c586d3e5ac1777192d9f1713e34d8d2
sophie36 <sophielemaire361@gmail.com>
2016-01-29 15:16:25 +0000

    tableau,table de hachage

---
+++ chapitre2/table_de_hachage.rb
@@ -0,0 +1,24 @@
+Les tables de hashage (ou hashes) permettent de ranger des données que l on retrouve à l aide d une clé.
+
+    'Pour créer une table de hashage, on utilise des accolades {}. Chaque valeur rangée dans cette table est 
+    associée à une clé qui permettra de la retrouver :'
+table_de_hashage = { clé_1: valeur_1, clé_2: valeur_2, ... }
+    
+    
+    'Par exemple, pour enregistrer le nombre de jours à passer dans chaque escale, je peux créer une table de 
+    hashage jours_voyage  :' 
+jours_voyage = { paris: 0, toronto: 7, nyc: 3 }
+
+
+    'Pour accéder à la valeur d un élément dans un hash, on précise sa clé entre crochets. Si je ne me souviens 
+    plus combien de temps j ai prévu pour mon escale à Toronto, il me suffit d appeler :'
+jours_voyage[:toronto]
+    'qui me renverra 7 jours.'
+    
+
+    'Pour ajouter ou modifier un élément dans un hash, on utilise l opérateur =. Par exemple en ajoutant :'  
+jours_voyage[:rio] = 5
+
+
+    'mon hash jours_voyage contiendra la durée de mon séjour à Rio en plus :' 
+{ paris: 0, toronto: 7, nyc: 3, rio: 5 }
---
+++ chapitre2/tableau.rb
@@ -0,0 +1,21 @@
+mes_escales_tour_du_monde = [ "Paris", "Toronto", "NYC", "Rio", "Sydney", "Hong-Kong", "Berlin" ]
+
+        nom_du_tableau.nom_de_la_methode
+        
+mes_escales_tour_du_monde.size 
+        'qui renverra 7, le nombre de ville'
+        
+mes_escales_tour_du_monde.reverse
+        'qui commencera par Berlin et tout le reste à l envers'
+
+mes_escales_tour_du_monde[2]
+        'qui renverra NYC qui est en position 3 Le premier élément dans un tableau a pour index 0 (et pas 1) ! 
+        C est le cas aussi dans beaucoup de langages de programmation.'
+        
+mes_escales_tour_du_monde << "Londres"
+        'Pour ajouter un élément dans un tableau, on utilise deux chevrons <<. Si je veux prolonger
+        mon tour du monde à Londres, je taperai donc : '
+        
+mes_escales_tour_du_monde[6] = "Barcelone"
+        'Pour modifier l élément d un tableau, on lui affecte la valeur souhaitée avec le signe =.
+        Je peux donc remplacer Berlin par Barcelone si je parle mieux l espagnol que l allemand :'
